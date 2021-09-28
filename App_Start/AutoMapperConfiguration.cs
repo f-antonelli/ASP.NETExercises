c62732b38c57aebad11d5f5efa15e18194aa57d4
Fede Antonelli <antonelli.fd@gmail.com>
2021-09-28 10:23:41 -0300

    Agregando paquetes NuGet

---
+++ App_Start/AutoMapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.App_Start
+{
+	public class AutoMapperConfiguration
+	{
+		public MapperConfiguration Configure()
+		{
+			var config = new MapperConfiguration(c =>
+			{
+				c.AddProfile<MappingProfile>();
+			});
+			return config;
+		}
+	}
+	
+}
--- Global.asax.cs
+++ Global.asax.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using AutoMapper;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Vidly.App_Start;
+
 
 namespace Vidly
 {
@@ -12,6 +16,7 @@ namespace Vidly
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
